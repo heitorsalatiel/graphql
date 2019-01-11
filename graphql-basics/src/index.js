6d6bf4f0d0c4f56d0df76b93ec31f807bb94bfb7
Heitor Salatiel <salatielheitor@gmail.com>
2019-01-10 23:46:08 -0200

    Creating custom objects

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,28 +1,84 @@
 import {GraphQLServer} from 'graphql-yoga';
 
+//Scalar Types = String, Boolean, Int/Float/Double, ID (unique identifiers)
+
 //Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
+        age: Int!
+        employed: Boolean!
+        gpa:Float
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
+        me: User!
+        post: Post!
+    }
+
+    type User{
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID
+        title: String!
+        pubilshed: Boolean!
     }
 `
 //Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my  first query!'
+        id() {
+            return 30;
         },
-        name() {
-            return 'Heitor Salatiel'
+        name(){
+            return 'Heitor';
         },
-        location() {
-            return 'BH'
+        age(){
+            return 27;
         },
-        bio() {
-            return 'I\'m a nice guy'
+        employed(){
+            return true;
+        },
+        gpa(){
+            return 3.5;
+        },
+        title(){
+            return 'acai';
+        },
+        price(){
+            return 31.50;
+        },
+        releaseYear(){
+            return 2019;
+        },
+        rating(){
+            return null;
+        },
+        inStock(){
+            return true;
+        },
+        me(){
+            return {
+                id: '2',
+                name: 'Heitor',
+                email: 'heitor@gmail.com',
+                age: '29'
+            }
+        },
+        post() {
+            return {
+                id: 23,
+                title: 'The life',
+                pubilshed: true
+            }
         }
     }
 }
