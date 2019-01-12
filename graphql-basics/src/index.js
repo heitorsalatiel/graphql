1a3794284b8304e5586e31e1e0086b147ca90771
Heitor Salatiel <salatielheitor@gmail.com>
2019-01-12 11:29:14 -0200

    Adding Comments Property

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -50,19 +50,27 @@ const posts = [
 const comments = [
     {
         id:'1',
-        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+        text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        author:'1',
+        post:'1'
     },
     {
         id:'2',
-        text:'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+        text:'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+        author:'2',
+        post:'2'
     },
     {
         id:'3',
-        text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
+        text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+        author:'3',
+        post:'3'
     },
     {
         id:'4',
-        text:'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.'
+        text:'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+        author:'1',
+        post:'3'
     }
 ]
 
@@ -101,6 +109,7 @@ const typeDefs = `
         email: String!
         age: Int
         posts:[Post!]!
+        comments:[Comment!]!
     }
 
     type Post {
@@ -109,11 +118,14 @@ const typeDefs = `
         body: String!
         pubilshed: Boolean!
         author: User!
+        comments:[Comment!]!
     }
 
     type Comment {
         id: ID!
         text: String!
+        author: User!
+        post: Post!
     }
 `
 //Resolvers
@@ -214,6 +226,11 @@ const resolvers = {
             return users.find((user,index) => {
                 return parent.author === user.id;
             })
+        },
+        comments(parent, args,ctx,info){
+            return comments.filter((comment,index) => {
+                return comment.post === parent.id;
+            })
         }
     },
     User:{
@@ -221,6 +238,23 @@ const resolvers = {
             return posts.filter((post,index) => {
                 return parent.id === post.author;
             });
+        },
+        comments(parent,args,ctx,info){
+            return comments.filter((comment,index) => {
+                return parent.id === comment.author;
+            })
+        }
+    },
+    Comment: {
+        author(parent,args,ctx,info) {
+            return users.find((user,index) => {
+                return user.id === parent.author;
+            })
+        },
+        post(parent,args,ctx,info) {
+            return posts.find((post,index) => {
+                return post.id === parent.post;
+            })
         }
     }
 }
