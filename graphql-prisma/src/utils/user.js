84c4e4b98b30d95be4b8791bfd38b31453bed313
Heitor Salatiel <salatielheitor@gmail.com>
2019-05-04 19:04:14 +0100

    Playing with dummy tests

---
+++ graphql-prisma/src/utils/user.js
@@ -0,0 +1,11 @@
+const GetFirstName = (fullName) => {
+    return fullName.split(' ')[0];
+}
+
+const IsValidPassword = (password) => {
+    var isValid = password.length >= 8 && !password.toLowerCase().includes('password');
+    return isValid;
+}
+
+ export { GetFirstName, IsValidPassword}; 
+
